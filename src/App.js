d30389acbf92ade85018a0ef8004b28d12a434a7
Malcolm Lynch <milynch1320@gmail.com>
2019-04-24 21:42:07 -0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-//Initial 
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path='/' component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/contact' component={Contact} />
   </div>
 );
 
